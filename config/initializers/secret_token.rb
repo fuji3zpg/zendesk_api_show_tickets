e6b567f1034de5281847fe6c8b0dcc92ad8c4ea5
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-04-22 14:23:09 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZendeskApiWithRails::Application.config.secret_key_base = '892250a6d9039190f49468cda0214f4632b1b1b92a36d6e43ac8a8d0680f73f86ae9a1c064dbb7c962bfc6e49c463392f22111298c93f1e83dee79f0f00a138d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZendeskApiWithRails::Application.config.session_store :cookie_store, key: '_zendesk_api_with_rails_session'
