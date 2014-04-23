cf309037df276e41c5d51f41e4a5437fe39d5ad3
Kazunori Fujimoto <kazu-f@sd5.so-net.ne.jp>
2014-04-23 13:21:00 +0900

    show first page and 10 tickets

--- app/controllers/zendesk_tickets_controller.rb
+++ app/controllers/zendesk_tickets_controller.rb
@@ -1,6 +1,6 @@
 class ZendeskTicketsController < ApplicationController
   def index
-    @tickets = ZendeskApiInit.client.tickets
+    @tickets = ZendeskApiInit.client.tickets.page(1).per_page(10) # 10チケットを取得して、最初の1ページ目を表示。pageメソッドが表示されるページで、per_pageメソッドがページに表示するデータ数。
   end
 
   def show
